a0342cd491164daf59419084ee2837b4cb2bb43f
Surefirer <46170182+Surefirer@users.noreply.github.com>
2020-09-22 15:31:06 -0700

    update

--- src/App.js
+++ src/App.js
@@ -1,14 +1,31 @@
 import React from "react";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 // import HelloWorld from "./Components/HelloWorld";
 //import CounterExample from "./Components/CounterExample";
 import Header from "./Components/Header";
 import Footer from "./Components/Footer";
+import Home from "./Components/Views/Home";
+import About from "./Components/Views/About";
 
 function App() {
   return (
     <div>
-      <Header />
-      <Footer />
+      <Router>
+        <Header />
+
+        <div className="p-3">
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/about">
+              <About />
+            </Route>
+          </Switch>
+        </div>
+
+        <Footer />
+      </Router>
     </div>
   );
 }
---
+++ src/Components/Views/Home.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+function Home() {
+  return (
+    <div>
+      <h1 className="font-bold text-2xl mt-3 mb-3">This is the home page</h1>
+    </div>
+  );
+}
+
+export default Home;
